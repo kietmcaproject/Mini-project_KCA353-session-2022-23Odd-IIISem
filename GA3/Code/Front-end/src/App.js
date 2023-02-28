8711aa3adf37f31d4eac925564314e752b3128d0
Abhinav Choudhary <89508503+abhinavchoudhary12@users.noreply.github.com>
2023-02-28 22:53:56 +0530

    Add files via upload

---
+++ GA3/Code/Front-end/src/App.js
@@ -0,0 +1,17 @@
+//import logo from './logo.svg';
+//import { BrowserRouter as Router } from 'react-router-dom';
+import './App.css';
+import MainApp from './components/MainApp';
+
+function App() {
+  return (
+    <>
+    
+    <MainApp/>
+    
+    
+      </>
+  );
+}
+
+export default App;
