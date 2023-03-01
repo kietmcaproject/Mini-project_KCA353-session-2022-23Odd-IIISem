386272bab29d4e28f1752b50b94dee0e16d60f50
Abhinav Tyagi <95855756+Abhinav1tyagi@users.noreply.github.com>
2023-03-01 12:58:02 +0530

    Delete Mini project related directory

--- Mini project related/bbdms/includes/footer.php
+++
@@ -1,8 +0,0 @@
-  
-  
-  <footer class="py-5 bg-inverse">
-        <div class="container">
-            <p class="m-0 text-center text-white">Copyright &copy; KLNAV-Way to Life 2022</p>
-        </div>
-        <!-- /.container -->
-    </footer>
